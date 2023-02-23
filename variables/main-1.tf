d3ef2ebee83432015effa20213f458db8444e3c7
wwyhy <wwyhy520@163.com>
2023-02-23 18:16:19 +0800

    Create main-1.tf

---
+++ variables/main-1.tf
@@ -0,0 +1,143 @@
+### the special is defined the variable with map type, and used by machine_type and environment(line 45/47)
+
+###-----------------------------------
+### PROVIDER
+provider "google" {
+  project = var.project-id
+  region  = var.region
+  zone    = var.zone
+}
+
+### NETWORK
+data "google_compute_network" "default" {
+  name                    = "default"
+}
+
+## SUBNET
+resource "google_compute_subnetwork" "subnet-1" {
+  name                     = var.subnet-name
+  ip_cidr_range            = var.subnet-cidr
+  network                  = data.google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = var.private_google_access
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = data.google_compute_network.default.self_link
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = var.firewall-ports
+  }
+
+  source_tags = var.compute-source-tags
+}
+
+### COMPUTE
+## NGINX PROXY
+resource "google_compute_instance" "nginx_instance" {
+  name         = "nginx-proxy"
+  machine_type = var.environment_machine_type[var.target_environment]
+  labels = {
+    environment = var.environment_map[var.target_environment]
+  }
+  tags = var.compute-source-tags
+ 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+    access_config {
+  
+    }
+  }
+}
+
+## WEB1
+resource "google_compute_instance" "web1" {
+  name         = "web1"
+  machine_type = var.environment_machine_type[var.target_environment]
+  labels = {
+    environment = var.environment_map[var.target_environment]
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+  }
+}
+## WEB2
+resource "google_compute_instance" "web2" {
+  name         = "web2"
+  machine_type = var.environment_machine_type[var.target_environment]
+  labels = {
+    environment = var.environment_map[var.target_environment]
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+  }
+}
+## WEB3
+resource "google_compute_instance" "web3" {
+  name         = "web3"
+  machine_type = var.environment_machine_type[var.target_environment]
+  labels = {
+    environment = var.environment_map[var.target_environment]
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+  }  
+}
+
+## DB
+resource "google_compute_instance" "mysqldb" {
+  name         = "mysqldb"
+  machine_type = var.environment_machine_type[var.target_environment]
+  labels = {
+    environment = var.environment_map[var.target_environment]
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+  }  
+}
